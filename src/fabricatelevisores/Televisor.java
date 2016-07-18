773628ec1ad360b28bc50b172c5238e145d6eaf5
123jhon <jajoya17@misena.edu.co>
2016-07-18 15:04:36 -0500

    // metodos tv

--- src/fabricatelevisores/Televisor.java
+++ src/fabricatelevisores/Televisor.java
@@ -11,44 +11,28 @@ import java.util.Scanner;
  *
  * @author alejozepol @
  */
-public class Televisor {
-    Scanner op=new Scanner(System.in);
-//METODOS
+public class Televisor {    Scanner op=new Scanner(System.in);
     
-    public void EncenderTv(String enc){
+     public void EncenderTv(String enc){
          if (enc.equals("on")) { 
          System.out.println("la tv esta encendida"); 
          }
      } 
-     String sw; 
-     
-    public void CambiarCanal(String elegir){
-         System.out.println("INGRESE NUMERO DE CANAL");
-     do{ String num; num=op.next(); 
-     if (num.equals("9")) System.out.println(">>>>>usted esta viendo INSTITUCIONAL"); 
-     if(num.equals("11")) System.out.println(">>>>>usted esta viendo TNT"); 
-     if(num.equals("13")) System.out.println(">>>>>usted esta viendo ATV"); 
-     if(num.equals("4")) System.out.println(">>>>>usted esta viendo RCN"); 
-     System.out.println("····presione !!SW!! para cambiar·····"); 
-     System.out.println("····presione SALIR para cambiar de opcion·······"); sw=op.next(); 
-     }
-     while(sw.equals("sw")); 
+
+     public void CantVolumen(int x){
+         if (x>10 && x<51) {
+             System.out.println("volumen MEDIO......:::");
+         } 
+         if(x>51 && x<101){ 
+             System.out.println("volumen ALTO......:::!!!!!¨¨"); 
+         }
+         if(x>0 && x<10){ 
+             System.out.println("volumen BAJO..::"); 
+         }
      } 
-     
-    void CantVolumen(int nextInt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void Mute(String next) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void CambiaraCable(String next) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void AccesoInternet(String next) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+     public void Mute(String mute){ 
+         if (mute.equals("mute")) { 
+             System.out.println("!!!MUTE!!!!");
+         }
+     }
 }
