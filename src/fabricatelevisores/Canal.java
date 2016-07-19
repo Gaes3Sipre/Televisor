196756fad27c0ee6a0a67e75fcdc6aa97f3fa960
123jhon <jajoya17@misena.edu.co>
2016-07-19 14:04:04 -0500

    Add files via upload

--- src/fabricatelevisores/Canal.java
+++ src/fabricatelevisores/Canal.java
@@ -12,63 +12,32 @@ import java.util.Scanner;
  * @author alejozepol
  */
 public class Canal {
+    Scanner op = new Scanner(System.in);
+    String sw;
 
-    byte numero;
-    String nombre;
-    String clasificacion;
-
-    static int i[] = new int[1];
-    static Scanner lectura = new Scanner(System.in);
-    static String[][] aire = new String[3][3];
-    static String[][] video1 = new String[4][4];
-
-//    metodo a modificar
-    public int CambiarCanal(int a) {
-        return i[a];
-    }
-
-    public void aire(boolean conectado) {
-        /*	boolean: la idea es que si esta en true
- * estos canasles se vean al inicia el tv 
- * */
-//        CambiarCanal();
-        aire[0][0] = "canal capital";
-        aire[0][1] = "caracol";
-        aire[0][2] = "rcn";
-        aire[1][0] = "cana constitucional";
-        aire[1][1] = "city tv";
-        aire[1][2] = "canal";
-        aire[2][0] = "canal";
-        aire[2][1] = "canal";
-        aire[2][2] = "canal";
-    }
-
-    public Canal(byte numero, String nombre, String clasificacion) {
-        this.numero = numero;
-        this.nombre = nombre;
-        this.clasificacion = clasificacion;
-    }
-
-    public void video1(boolean conectado) {
-        /*la idea es que estos canales le agregues el tipo de canal
- * */
-
-        video1[0][0] = "canal ";
-        video1[0][1] = "canal";
-        video1[0][2] = "canal";
-        video1[0][3] = "canal";
-        video1[1][0] = "canal";
-        video1[1][1] = "canal";
-        video1[1][2] = "canal";
-        video1[1][3] = "canal";
-        video1[2][0] = "canal";
-        video1[2][1] = "canal";
-        video1[2][2] = "canal";
-        video1[2][3] = "canal";
-        video1[3][0] = "canal";
-        video1[3][1] = "canal";
-        video1[3][2] = "canal";
-        video1[3][3] = "canal";
+    public void CambiarCanal(String elegir) {
+        if (elegir.equals("ELEGIR")) {
+            System.out.println("INGRESE NUMERO DE CANAL");
+            do {
+                String num;
+                num = op.next();
+                if (num.equals("9")) {
+                    System.out.println(">>>>>usted esta viendo CANAL INSTITUCIONAL");
+                }
+                if (num.equals("11")) {
+                    System.out.println(">>>>>usted esta viendo SEÑAL COLOMBIA");
+                }
+                if (num.equals("13")) {
+                    System.out.println(">>>>>usted esta viendo CANAL 13");
+                }
+                if (num.equals("4")) {
+                    System.out.println(">>>>>usted esta viendo RCN");
+                }
+                System.out.println("····presione !!SW!! para cambiar·····");
+                System.out.println("····presione SALIR para cambiar de opcion·······");
+                sw = op.next();
+            } while (sw.equals("sw"));
+        }
     }
 
 }
